6b90712e85984fa1d8e0cc08cc0d30e94ed89643
BalaPriyaMS <balapriya1701@gmail.com>
2025-10-29 16:40:59 +0530

    setup the shadcn componet and global font family

--- client/src/modules/auth/login-page.tsx
+++ client/src/modules/auth/login-page.tsx
@@ -1,3 +1,11 @@
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+
 export const LoginPage = () => {
-  return <div>form will come</div>;
+  return (
+    <Card className="flex justify-center items-center shadow-none p-4 border-none w-3/5 h-96">
+      <p className="font-bold text-3xl">Welcome !</p>
+      <Button>login</Button>
+    </Card>
+  );
 };
